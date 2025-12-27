7671ef4f5df0b7e445dbc4485a0a97f0d3f8c914
Tejas5007 <tejas.k9960960007@gmail.com>
2025-12-27 19:52:55 +0530

    Rename security_group variable to aws_security_group

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "instance_type" {
     default     =  "t2.micro" 
 }
 
-variable "security_group" { 
+variable "aws_security_group" { 
     description = "Name of security group" 
     default     = "jenkins-sg-2022" 
 }
